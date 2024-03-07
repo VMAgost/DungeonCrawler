375747cf2c8de77d32d0b4b92a824edc61a1fb89
bftsgt <bfutasz@gmail.com>
2024-03-07 10:42:09 +0100

    player sprite changes depending on hasSword

--- src/main/java/com/codecool/dungeoncrawl/data/mapObjects/Door.java
+++ src/main/java/com/codecool/dungeoncrawl/data/mapObjects/Door.java
@@ -7,6 +7,7 @@ import com.codecool.dungeoncrawl.data.Drawable;
 public class Door implements Drawable {
 
     private boolean isOpen = false;
+    private int breakAttempt = 0;
 
     private String name = "closed door";
 
@@ -31,5 +32,19 @@ public class Door implements Drawable {
         isOpen = true;
         name = "open door";
         cell.setType(CellType.FLOOR);
+    }
+
+    public void breakOpen(){
+        System.out.println("You broke the door open");
+        isOpen = true;
+        name = "broken door";
+        cell.setType(CellType.FLOOR);
+    }
+
+    public void attackDoor(){
+        this.breakAttempt++;
+    }
+    public int getBreakAttempts(){
+        return this.breakAttempt;
     }
 }

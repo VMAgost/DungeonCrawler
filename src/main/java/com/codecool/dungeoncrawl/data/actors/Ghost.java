2751464debb87c721065572f48d77e557970265d
Zsombor Pocs <142238433+Zsombinszky@users.noreply.github.com>
2024-03-07 08:58:22 +0100

    ready to merge

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
@@ -2,6 +2,8 @@ package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
 
+import java.util.Random;
+
 public class Ghost extends Enemy {
     public Ghost(Cell cell) {
         super(cell,4, 15);
@@ -11,4 +13,28 @@ public class Ghost extends Enemy {
     public String getTileName() {
         return "ghost";
     }
+
+    @Override
+    public void move(int dx, int dy) {
+        Random random = new Random();
+        int direction = random.nextInt(4);
+
+        switch (direction) {
+            case 0: // Move left
+                super.move(-1, 0);
+                break;
+            case 1: // Move right
+                super.move(1, 0);
+                break;
+            case 2: // Move up
+                super.move(0, -1);
+                break;
+            case 3: // Move down
+                super.move(0, 1);
+                break;
+            default:
+                break;
+        }
+    }
 }
+
